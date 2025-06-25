6e4d42f211ac901fbfc99b65236064c8a1492fee
Nurbek Juraev <powerzver98@gmail.com>
2025-06-25 09:23:07 +0500

    update

--- mini_app/settings.py
+++ mini_app/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/test/mini_app/media'
 
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
