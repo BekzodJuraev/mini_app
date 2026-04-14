eb4cf1209249ad264f2068af944b577f71047794
Bekzod <powerzver98@gmail.com>
2026-04-14 10:51:31 +0500

    update

--- backend/models.py
+++ backend/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     gender=models.CharField(max_length=200, null=True, blank=True,default=None)
     place_of_residence=models.CharField(max_length=200, null=True, blank=True,default=None)
     date_birth=models.DateField(null=True,blank=True,default=None)
-    photo = models.CharField(max_length=400,blank=True)
+    photo = models.ImageField(blank=True, upload_to='pictures/')
     balance=models.IntegerField(default=0)
     health_system=models.JSONField(null=True,default=None)
     life_expectancy=models.IntegerField(null=True,default=None,blank=True)
