db7c456aec10d285527fa2534d06bd37ce842c6a
Bekzod <powerzver98@gmail.com>
2026-05-12 11:57:08 +0500

    update

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Profile,Categories_Quest,Quest,Tests,Chat,Habit,Tracking_Habit,Drugs,Check_Drugs,Daily_check,Rentgen,Rentgen_Image,Pet,Calories,PetChat,Pet_Drugs,Pet_Check_Drugs,PetDaily_check,PetRentgen,PetRentgen_Image,Test, Question, Choice,Notification_drugs,NutritionGoal,Notification,NutritionGoalPet,PetCalories,Notification_Pet_drugs
+from .models import Profile,Categories_Quest,Quest,Tests,Chat,Habit,Tracking_Habit,Drugs,Check_Drugs,Daily_check,Rentgen,Rentgen_Image,Pet,Calories,PetChat,Pet_Drugs,Pet_Check_Drugs,PetDaily_check,PetRentgen,PetRentgen_Image,Test, Question, Choice,Notification_drugs,NutritionGoal,Notification,NutritionGoalPet,PetCalories,Notification_Pet_drugs,Tests_Pet
+
 import nested_admin
 
+@admin.register(Tests_Pet)
+class Tests_Pet(admin.ModelAdmin):
+    pass
 @admin.register(Notification_Pet_drugs)
 class Notification_Pet_drugs(admin.ModelAdmin):
     pass
